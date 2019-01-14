0ada19b97f785bcaabfe2ef1ff1f5567b7504765
Aurélien boncenne <aurelien.boncenne@gmail.com>
2019-01-14 16:19:54 +0100

    restart server

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var socket = require('socket.socket')(http);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var ent = require('ent');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
+var jsonParser = bodyParser.json()
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-
-app.get('/', functsocketn(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/connexion.html');
 });
-app.post('/chat',urlencodedParser,  urlencodedParser, function (req, res){
-    console.log('lol')
+app.get('/chat', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-socket.on('connection', function(socket, pseudo){
-
-  socket.on('chat message', function(message){
-    message = ent.encode(message);
-    socket.broadcast.emit('chat message', {pseudo: socket.pseudo, message: message});
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+/*
+var app = require('express')();
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var ent = require('ent');
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/connexion.html');
+});
+app.post('/chat', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+socket.on('connection', function(socket, pseudo){
+  socket.on('connect', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('connect', pseudo);
+    });
+
+
+  socket.on('chat message', function(message){
+      message = ent.encode(message);
+      socket.emit('chat message', msg);
+  });
+});
+*/
