f6638d2cf3acc629914ef7040bffc72c32a4506b
Kevin <kbreda73@gmail.com>
2019-01-14 14:00:20 +0100

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -16,3 +16,5 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+//卐卐
