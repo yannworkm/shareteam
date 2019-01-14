c9ce937121ee7da892a5863e4387088cbb776308
Aurélien boncenne <aurelien.boncenne@gmail.com>
2019-01-14 14:37:58 +0100

    fix merge app.js

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/connexion.html');
 });
 app.get('/chat', function(req, res) {
   res.sendFile(__dirname + '/chat.html');
 });
-=======
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
->>>>>>> ee880086251d97a212d4ff98ee99d9fd91af9da1
 
 io.on('connection', function(socket){
   socket.on('connection')  function(name,city,age){
