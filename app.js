51a02d0d97eccbc53198a42f329a6c7e5b3d34c9
Aurélien boncenne <aurelien.boncenne@gmail.com>
2019-01-15 13:14:29 +0100

    comment

--- app.js
+++ app.js
@@ -5,24 +5,30 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var ent = require('ent');
 var bodyParser = require('body-parser');
-
 var jsonParser = bodyParser.json();
 
+// Encodage des variables inter pages
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+// affichage du css
 app.use(express.static('public'));
 
-
+// route connexion
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/connexion.html');
 });
+
+// route chat
 app.post('/chat', function(req, res) {
-    var username = req.body.pseudo;
-    console.log(username);
+    //var username = req.body.pseudo;
+    //console.log(username);
     res.sendFile(__dirname + '/index.html');
 });
+
+// socket permettant d'afficher le chat
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
