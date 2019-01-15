8056d43d2a80ba57e366079d66addc0e91fa8503
Aurélien boncenne <aurelien.boncenne@gmail.com>
2019-01-15 11:08:41 +0100

    fix pretty

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -6,23 +7,20 @@ var ent = require('ent');
 var bodyParser = require('body-parser');
 
 var jsonParser = bodyParser.json();
-app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/connexion.html');
 });
-<<<<<<< HEAD
-
-app.get('/chat', function(req, res) {
-=======
 app.post('/chat', function(req, res) {
     var username = req.body.pseudo;
     console.log(username);
->>>>>>> 91458415e3a5259e553959ecb08f2191863f3916
     res.sendFile(__dirname + '/index.html');
 });
 
